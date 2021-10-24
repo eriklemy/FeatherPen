4dff75042ad61373a4a920ba958bd00e4dc93b6b
eriklemy <eriklemy2010@gmail.com>
2021-10-23 22:39:08 -0300

    Update

--- scripts/main-screen.js
+++ scripts/main-screen.js
@@ -8,29 +8,3 @@ window.onload = function(){
 function mudarPaginaCart(){
     window.location.href = "produtos.html";
 }
-
-var menuNavbar = document.getElementById("menuNavbar");
-menuNavbar.style.maxHeight = "0px";
-
-function menuToggle(){
-    if(menuNavbar.style.maxHeight == "0px"){
-        menuNavbar.style.maxHeight = "200px";
-    } else {
-        menuNavbar.style.maxHeight = "0px";
-    }
-}
-
-var theme = [];
-alert(theme[0])
-
-const theme = localStorage.getItem('dark-mode');
-if(theme) {
-    darkModeToggle(theme)
-}
-
-function darkModeToggle() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-    theme.push(document.body.classList.toggle("dark-mode"));
-    window.localStorage.setItem("dark-mode", JSON.stringify(theme));
-}
