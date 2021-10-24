5f528fe2888b60e9ec2ecea438528aa82a2e11e1
eriklemy <eriklemy2010@gmail.com>
2021-10-23 23:26:36 -0300

    ta no caminho

--- scripts/main-screen.js
+++ scripts/main-screen.js
@@ -7,6 +7,15 @@ var listaProdutos = [[0, 'O preço da verdade', 'preco_da_verdade.jpg', false],
                      [6, 'Dunkirk', 'dunkirk.jpg', true],
                      [7, 'Resgate', 'resgate.jpg', false]];
 
+var lista = [];
+window.onload = function(){
+    for(var i = 0; i < listaProdutos.length; i++){
+        lista.push(listaProdutos[i]);
+        window.localStorage.setItem("produtos", JSON.stringify(lista));
+    }
+}
+
 function mudarPaginaCart(){
+    // converter para JSON antes de colocar no storage p/reconhecer como objeto
     window.location.href = "produtos.html";
 }
--- scripts/produto-screen.js
+++ scripts/produto-screen.js
@@ -2,14 +2,42 @@ listaProdutos = [];
 var carrinho = [];
 // ao carregar a pagina
 window.onload = function(){
-    listaProdutos = JSON.parse(window.localStorage.getItem("carrinho"));
+    listaProdutos = JSON.parse(window.localStorage.getItem("produtos"));
     checkInCart();
-    montarCardProdutos();
+    viewMostSell();
 }
 
 function montarCardProdutos(){
     document.getElementById("div-produtos").innerHTML = "";
     for(var i = 0; i < listaProdutos.length; i++){
+        var conteudo = "";
+        conteudo += '<div class = "div-card">';
+        conteudo += '<div class = "div-card-img">';
+        conteudo += '<img src = "images/' + listaProdutos[i][2] + '" />';
+        conteudo += '</div>';
+        conteudo += '<div class = "div-card-description">';
+        conteudo +=  'Filme: ' + listaProdutos[i][1];
+        conteudo += '</div>';
+
+        if(listaProdutos[i][3] == false){
+            conteudo += '<div class = div-card-button-comprar onclick = "comprar(' + listaProdutos[i][0] + ')">';
+            conteudo += 'Adicionar ao carrinho';
+            conteudo += '</div>';    
+        } else {
+            conteudo += '<div class = div-card-button-carrinho>';
+            conteudo += 'produto ja no carrinho';
+            conteudo += '</div>';    
+        }
+        conteudo += '</div>';
+
+        document.getElementById("div-produtos").innerHTML += conteudo;
+    }
+}
+
+
+function viewMostSell(){
+    document.getElementById("div-produtos").innerHTML = "";
+    for(var i = 0; i < 4; i++){
         var conteudo = "";
         conteudo += '<div class = "div-card">';
         conteudo += '<div class = "div-card-img">';
