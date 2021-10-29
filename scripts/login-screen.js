fa11b5ec5b59a6c482c152d6916c2b39b6853e88
eriklemy <eriklemy2010@gmail.com>
2021-10-29 18:20:16 -0300

    tela de login em funcionamento

--- scripts/login-screen.js
+++ scripts/login-screen.js
@@ -9,16 +9,17 @@ if(darkMode === 'enabled'){
     localStorage.setItem('darkMode', 'enabled');
 }
 
+const entrar = document.querySelector('#entrar');
 
-function acessar(){
-    var nome = document.getElementById("nome").value;
+entrar.addEventListener('click', () => {
+    var nome = document.getElementById("username").value;
     var senha = document.getElementById("senha").value;
 
     console.log(nome);
     console.log(senha);
 
-    if(nome == cadastro[0] && senha == cadastro[4]){
+    if(nome === cadastro[3] && senha === cadastro[4]){
         alert("login realizado com sucesso!!");
         window.location.href = "produtos.html";
     } else alert("usuario ou senha incorreto!!");
-}
+});
